dd31a435ddffd517ac9ce1f46ef5779704d46ed8
Alex Lehmann <44248708+LexLehmann@users.noreply.github.com>
2019-05-19 09:58:29 -0400

    Update Election.py

--- Election.py
+++ Election.py
@@ -3,6 +3,10 @@ from Vote import Vote
 from Vote import Candidate
 ##from Vote import Tree
 
+## Takes a vote and sends it to its next choice.
+# If there is a tie for first place it splits the vote into
+# several fraction of votes. then puts those votes in their
+# respective candidates
 def distributeNewVotes(voter):
     for vote in voter.getList()[0]:
         newVote = voter.makeCopy()
@@ -17,13 +21,14 @@ def distributeNewVotes(voter):
         if candidates[vote].getAccp() < 1:
             nextIter = newVote.makeCopy()
             newVote.cutWeight(candidates[vote].getAccp())
-            nextIter.setWeight(voter.getWeight()- newVote.getWeight())
+            nextIter.setWeight(nextIter.getWeight()- newVote.getWeight())
             distributeNewVotes(nextIter)
 
         if newVote.getWeight() > 0:
             candidates[vote].addNewVoter(newVote)
 
-
+## Removes the last place candidate from the running and
+# sends their votes to the next choice
 def removeLowest():
     minVotes = -1
     for person in candidates:
@@ -36,6 +41,10 @@ def removeLowest():
         copy = vote.makeCopy()
         distributeNewVotes(copy)
 
+## Given a level to cut it down to, this will remove the fraction
+# needed to get the total count down to that amount from all of the votes
+# of all of the candidates over that amount. Then sends those fraction of
+# votes to their next choice
 def cutTop(threshold):
     toDistribute = []
     for person in candidates:
@@ -48,27 +57,33 @@ def cutTop(threshold):
     for vote in toDistribute:
         distributeNewVotes(vote)
 
+## This is the main method that runs the election.
+# The seats variable is how you set how many options can win the election
+# additionalQuota is how much the threshold should be above the minimum amount
 def election():
-    Seats = 3
-    threshold = Fraction(len(votes), Seats + 1) + Fraction(1,1000)
+    seats = 5
+    additionalQuota = Fraction(0,1000)
+    accuracy = Fraction(1,100)
+    threshold = Fraction(len(votes), seats + 1) + additionalQuota
     candidatesLeft = len(candidates)
 
-    while candidatesLeft > Seats + 1:
+    while candidatesLeft > seats + 1:
         cutTopAgain = True
         while cutTopAgain:
             cutTopAgain = False
             for person in candidates:
-                if person.getCount() > threshold:
+                if person.getCount() > threshold + accuracy:
                     cutTopAgain = True
             if cutTopAgain:
                 cutTop(threshold)
 
         removeLowest()
         candidatesLeft -= 1
+        print("one down")
 
     lastRemoval = 0
     for person in candidates:
-        if lastRemoval == 0 or (person.getCount() < lastRemoval.getCount() and person.getAccp() > 0):
+        if person.getAccp() > 0 and (lastRemoval == 0 or person.getCount() < lastRemoval.getCount()):
             lastRemoval = person
 
     lastRemoval.finalRemoval()
@@ -76,6 +91,10 @@ def election():
 
 
 ## START ##
+## reads in a txt file with v lines of c integers
+# where v is the number of voters and c is the number of candidates
+# The lowest integer is considered the most preferred choice.
+# Ties are allowed. All integers over 10000 will be considered a tie for last place
 inputFile = open("votes.txt", "r")
 input = []
 for line in inputFile:
