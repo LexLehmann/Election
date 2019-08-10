1cc83facdafeda2877f3ce364e9256175392d645
Alex Lehmann <44248708+LexLehmann@users.noreply.github.com>
2019-08-10 11:20:48 -0400

    Update Copeland.py

--- Copeland.py
+++ Copeland.py
@@ -5,46 +5,33 @@ from Vote import Candidate
 class Copeland:
 
     def run(self, input):
-        votes = input
+        count = 0
+        Scores = []
+        for line in input:
+            i = 0
+            for outcome in line:
+                if outcome == 1:
+                    i += 1
+            pair = [count, i]
+            Scores.append(pair)
+            count += 1
 
-        i = 0
-        candidates = []
-        for tie in input[0].list:
-            for option in tie:
-                candidates.append(Candidate(i))
+        sorted = []
+        for item in Scores:
+            i = 0
+            while(i < len(sorted) and item[1] < sorted[i][1]):
                 i += 1
+            if i < len(sorted) and item[1] == sorted[i][1]:
+                if isinstance(sorted[i][0], int):
+                    sorted[i][0] = [item[0], sorted[i][0]]
+                else:
+                    sorted[i][1].append(item[i])
+            else:
+                sorted.insert(i, item)
 
-        table = []
-        for row in candidates:
-            line = []
-            for col in candidates:
-                line.append(0)
-            table.append(line)
+        output = []
+        for pair in sorted:
+            output.append(pair[0])
 
-        i = 1
-        for person in candidates:
-            j = i
-            while(j < len(candidates)):
-                count = 0
-                for vote in votes:
-                    counted = False
-                    k = 0
-                    while not counted:
-                        for choice in vote.getList()[k]:
-                            if choice == person.getIdent():
-                                counted = True
-                                count += 1
-                            if choice == j:
-                                counted = True
-                                count -= 1
-                        k += 1
 
-                if count > 0:
-                    table[person.getIdent()][j] = 1
-                    table[j][person.getIdent()] = -1
-                elif count < 0:
-                    table[person.getIdent()][j] = -1
-                    table[j][person.getIdent()] = 1
-                j += 1
-            i += 1
-        print(table)
+        print("Copeland: " + str(output))
