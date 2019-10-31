87270ed9dc1e2b639cad932d71e60a8b51a694ac
Alex Lehmann <44248708+LexLehmann@users.noreply.github.com>
2019-10-30 22:53:25 -0400

    Update ElectionMain.py

--- ElectionMain.py
+++ ElectionMain.py
@@ -2,6 +2,7 @@ from meekSTV import Meek
 from Borda import Borda
 from Vote import Vote
 from Copeland import Copeland
+from Condorcet import Condorcet
 from TournamentTable import TournamentTable
 from F1 import Formula1
 from Plurality import Plurality
@@ -9,6 +10,7 @@ from Plurality import ReversePlurality
 from Minimax import Minimax
 from Baldwin import Baldwin
 from Bucklin import Bucklin
+from Contingent import Contingent
 
 inputFile = open("votes.txt", "r")
 input = []
@@ -38,37 +40,69 @@ for voter in input:
     thisVote = Vote(thisRank)
     votes.append(thisVote)
 
+TournamentTable = TournamentTable()
+pairTable = TournamentTable.run(votes)
+tourtable = TournamentTable.simplify(pairTable)
+del(TournamentTable)
+
 Plur = Plurality()
 print("Plurality: " + str(Plur.run(votes)))
 
-meek = Meek()
-meek.run(votes)
+print("Plurality with Removal: " + str(Plur.runWithRemoval(votes)))
+del(Plur)
 
-borda = Borda()
-print("Borda: " + str(borda.run(votes)))
+Borda = Borda()
+print("Borda: " + str(Borda.run(votes)))
+
+print("Borda with Removal: " + str(Borda.runWithRemoval(votes)))
+del(Borda)
 
 f1 = Formula1()
-f1.run(votes)
-
-TournamentTable = TournamentTable()
-table = TournamentTable.run(votes)
+print("Formula 1: " + str(f1.run(votes)))
+del(f1)
 
 RPlur = ReversePlurality()
 print("Reverse Plurality: " + str(RPlur.run(votes)))
 
+print("Reverse Plurality with Removal: " + str(RPlur.runWithRemoval(votes)))
+del(RPlur)
+
+condorcet = Condorcet()
+print("Condorcet: " + str(condorcet.run(tourtable)))
+del(condorcet)
+
 cope = Copeland()
-cope.run(table)
+print("Copeland: " + str(cope.run(tourtable)))
+
+print("Copeland with Removal: " + str(cope.runWithRemoval(tourtable)))
+del(cope)
 
 mm = Minimax()
-mm.run(votes)
+print("Minimax: " + str(mm.run(pairTable)))
 
-bucklin = Bucklin()
-print("Bucklin: " + str(bucklin.run(votes)))
+print("Minimax with Removal: " + str(mm.runWithRemoval(pairTable)))
+del(mm)
 
 baldwin = Baldwin()
 print("Baldwin: " + str(baldwin.run(votes)))
 
-contingent = Contingent()
-print("Contingent: " + str(contingent.run(votes, table)))
+print("Baldwin With Removal: " + str(baldwin.runWithRemoval(votes)))
+del(baldwin)
 
-print(table)
+contingent = Contingent()
+print("Contingent: " + str(contingent.run(votes)))
+
+meek = Meek()
+print("Meek STV: " + str(meek.run(votes, 2)))
+
+bucklin = Bucklin()
+print("Bucklin with 50% threshold: " + str(bucklin.run(votes, 1)))
+
+print("---- requires number of seats ----")
+seats = 2
+
+#meek = Meek()
+#print("Meek STV: " + str(meek.run(votes, seats)))
+
+bucklin = Bucklin()
+print("Bucklin: " + str(bucklin.run(votes, seats)))
